c6e1a415515a0359b861f59afb2ff6a47c2a662f
devinrankin <devinrankin@temple.edu>
2023-09-03 07:38:22 -0400

    changed anchor to next link tags

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import styles from '../styles/Navbar.module.css'
 
 // FA icons for navbar
@@ -14,22 +15,22 @@ export default function Navbar({}: Props) {
                 <h1 className={styles.long}>devin rankin</h1>
             </li>
             <li className={styles.item}>
-                <a href="/about" className={styles.link}>
+                <Link href="/about" className={styles.link}>
                 <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" className={styles.faprimary}></path></svg>
                 <span className={styles.text}>About Me</span>
-                </a>
+                </Link>
             </li>
             <li className={styles.item}>
-                <a href="/projects" className={styles.link}>
+                <Link href="/projects" className={styles.link}>
                 <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z" className={styles.faprimary}></path></svg>
                 <span className={styles.text}>My Projects</span>
-                </a>
+                </Link>
             </li>
             <li className={styles.item}>
-                <a href="/resume" className={styles.link}>
+                <Link href="/resume" className={styles.link}>
                 <svg aria-hidden="true" focusable="false" data-prefix="fad" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" className={styles.faprimary}></path></svg>
                 <span className={styles.text}>Resume</span>
-                </a>
+                </Link>
             </li>
             <li className={styles.item}>
                 <a href="mailto:devinrankin@temple.edu" target="_blank" className={styles.link}>
