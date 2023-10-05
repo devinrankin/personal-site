f7c69a7d286841a899b08205d69f14f2256ab6b4
Devin Rankin <devinrankin@temple.edu>
2023-10-05 15:43:19 -0400

    widened about cards on mobile

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,72 +1,79 @@
-"use client"
-import React from 'react'
-import Bold from '../bold'
-import Card from './card/Card'
+"use client";
+import React from "react";
+import Bold from "../bold";
+import Card from "./card/Card";
 
+type Props = {};
 
-type Props = {}
-
-const createVariants = ( xInitial : number, xChange : number, duration : number ) => {
-    return {
-        visible: {
-            opacity: 1,
-            x: xChange,
-            transition: {
-                duration: duration,
-                delay: 0.2,
-                type: 'spring'
-            }
-        }, 
-        hidden: {
-            opacity: 0,
-            x: xInitial
-        }
-    }
-}
+const createVariants = (
+  xInitial: number,
+  xChange: number,
+  duration: number
+) => {
+  return {
+    visible: {
+      opacity: 1,
+      x: xChange,
+      transition: {
+        duration: duration,
+        delay: 0.2,
+        type: "spring",
+      },
+    },
+    hidden: {
+      opacity: 0,
+      x: xInitial,
+    },
+  };
+};
 
 export default function About({}: Props) {
   return (
     <section className="min-h-screen max-h-max flex flex-col" id="about">
-        <div className='p-16 sm:ml-60'>
-            <Card title='about me' variants={createVariants(240, 0, 1.5)}>
-                <p className='pt-4 text-typography'>
-                    in high school, i was introduced to computer science through game development, and i instantly fell in love. i enrolled into <Bold>temple university</Bold> majoring in computer science. there, my passion for computing grew.
-                </p>
-            </Card>
-            <Card title='interests' variants={createVariants(-240, 0, 1.5)}>
-                <p className='pt-4 text-typography'>
-                    currently interested in
-                </p>
-                <ul className='list-disc pl-8 text-typography'>
-                    <li>web development</li>
-                    <li>ui/ux design</li>
-                    <li>computer networking</li>
-                    <li>machine learning</li>
-                </ul>
-            </Card>
-            <Card title='hobbies' variants={createVariants(200, 0, 1.5)}>
-                <ul className='list-disc pl-8 pt-4 text-typography'>
-                    <li>video games</li>
-                    <li>learning chinese</li>
-                    <li>anime</li>
-                    <li>manga</li>
-                    <li>music</li>
-                    <li>cooking</li>
-                </ul>
-            </Card>
-            <Card title='experience' variants={createVariants(-200, 0, 1.5)}>
-                <ul className='list-disc pl-8 pt-4 text-typography'>
-                    <li><Bold>temple university</Bold>
-                        <ul className='list-disc pl-2 md:pl-8'>
-                            <li>teaching assistant - introduction to java</li>
-                            <li>teaching assistant - low-level system programming</li>
-                            <li>computer science peer tutor</li>
-                        </ul>
-                    </li>
-                    <li><Bold>coventry life settlements</Bold> - software engineer intern</li>
-                </ul>
-            </Card>
-        </div>
+      <div className="p-16 sm:ml-60">
+        <Card title="about me" variants={createVariants(240, 0, 1.5)}>
+          <p className="pt-4 text-typography">
+            in high school, i was introduced to computer science through game
+            development, and i instantly fell in love. i enrolled into{" "}
+            <Bold>temple university</Bold> majoring in computer science. there,
+            my passion for computing grew.
+          </p>
+        </Card>
+        <Card title="interests" variants={createVariants(-240, 0, 1.5)}>
+          <p className="pt-4 text-typography">currently interested in</p>
+          <ul className="list-disc pl-8 text-typography">
+            <li>web development</li>
+            <li>ui/ux design</li>
+            <li>computer networking</li>
+            <li>machine learning</li>
+          </ul>
+        </Card>
+        <Card title="hobbies" variants={createVariants(200, 0, 1.5)}>
+          <ul className="list-disc pl-8 pt-4 text-typography">
+            <li>video games</li>
+            <li>learning chinese</li>
+            <li>anime</li>
+            <li>manga</li>
+            <li>music</li>
+            <li>cooking</li>
+          </ul>
+        </Card>
+        <Card title="experience" variants={createVariants(-200, 0, 1.5)}>
+          <ul className="list-disc pl-8 pt-4 text-typography">
+            <li>
+              <Bold>temple university</Bold>
+              <ul className="list-disc pl-2 md:pl-8">
+                <li>teaching assistant - introduction to java</li>
+                <li>teaching assistant - low-level system programming</li>
+                <li>computer science peer tutor</li>
+              </ul>
+            </li>
+            <li>
+              <Bold>coventry life settlements</Bold> - software engineer intern
+            </li>
+          </ul>
+        </Card>
+      </div>
     </section>
-  )
+  );
 }
--- app/components/about/card/Card.tsx
+++ app/components/about/card/Card.tsx
@@ -1,24 +1,23 @@
-import React, { HTMLAttributes, ReactNode } from 'react'
-import { Variants, motion } from 'framer-motion'
+import React, { HTMLAttributes, ReactNode } from "react";
+import { Variants, motion } from "framer-motion";
 
 type Props = {
-    title: string,
-    children: ReactNode,
-    variants: Variants
-}
+  title: string;
+  children: ReactNode;
+  variants: Variants;
+};
 
-export default function Card({ title, children, variants} : Props) {
+export default function Card({ title, children, variants }: Props) {
   return (
-    <motion.div 
-        className='rounded-xl shadow-2xl flex flex-col bg-secondary text-bold-typography text-xl px-8 py-4 leading-8 mt-12 max-w-[45rem] md:ml-32 md:w-1/2'
-        variants={variants}
-        initial='hidden'
-        whileInView='visible'
-        viewport={{ once: true }}>
-            <h1 className='text-2xl pt-4 text-bold-typography font-black'>
-                {title}
-            </h1>
-            <div className='text-typography text-xl mb-4'>{children}</div>
+    <motion.div
+      className="rounded-xl shadow-2xl flex flex-col bg-secondary text-bold-typography text-xl px-8 py-4 leading-8 mt-12 max-w-[45rem] md:ml-32 md:w-1/2"
+      variants={variants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      <h1 className="text-2xl pt-4 text-bold-typography font-black">{title}</h1>
+      <div className="text-typography text-xl mb-4">{children}</div>
     </motion.div>
-  )
+  );
 }
