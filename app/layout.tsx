1402a5686140bfd268c044e03d6d0d99bc5e1e98
devinrankin <devinrankin@temple.edu>
2023-10-12 21:00:33 -0400

    added head element to display favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,44 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import "./globals.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import Head from "next/head";
 
 const ceraRound = localFont({
   src: [
     {
-      path: '../public/font/CeraRoundPro-Regular.woff2',
-      weight: '400',
-      style: 'normal'
+      path: "../public/font/CeraRoundPro-Regular.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../public/font/CeraRoundPro-Bold.woff2',
-      weight: '700',
-      style: 'normal'
+      path: "../public/font/CeraRoundPro-Bold.woff2",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../public/font/CeraRoundPro-Black.woff2',
-      weight: '800',
-      style: 'normal'
+      path: "../public/font/CeraRoundPro-Black.woff2",
+      weight: "800",
+      style: "normal",
     },
   ],
-}); 
+});
 
 export const metadata: Metadata = {
-  title: 'div',
-  description: '',
-}
+  title: "div",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="../public/favicon.png" />
+      </Head>
       <body className={ceraRound.className}>{children}</body>
     </html>
-  )
+  );
 }
