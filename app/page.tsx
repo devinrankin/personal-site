1abe048b87b911cd90a9dc73e0a43c4f266e622a
Devin Rankin <103450760+devinrankin@users.noreply.github.com>
2023-10-17 11:42:14 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Navbar from "./components/navbar/navbar";
 import Intro from "./components/intro/intro";
 import About from "./components/about/about";
 import Skills from "./components/skills/skills";
-import Experience from "./components/experience/experience";
 import Projects from "./components/projects/projects";
 import Timeline from "./components/timeline/timeline";
 import { ThemeProvider } from "next-themes";
