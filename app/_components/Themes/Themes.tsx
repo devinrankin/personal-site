be19fc853d8427dbd5f476484f2941a409e70e86
Devin Rankin <tun80253@temple.edu>
2024-04-17 19:04:16 -0400

    theme display style changing

--- app/_components/Themes/Themes.tsx
+++ app/_components/Themes/Themes.tsx
@@ -9,39 +9,45 @@ export default function Themes({}: Props) {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="border-typography border-2 rounded-2xl p-2 flex space-x-4 items-center justify-center">
-      <ThemeTooltip title={themes[0].title}>
-        <div
-          className="rounded-full bg-[#EF6A80] h-6 w-6"
-          onClick={() => {
-            setTheme(themes[0].title);
-          }}
-        />
-      </ThemeTooltip>
-      <ThemeTooltip title={themes[1].title}>
-        <div
-          className="rounded-full bg-[#436FC7] h-6 w-6"
-          onClick={() => {
-            setTheme(themes[1].title);
-          }}
-        />
-      </ThemeTooltip>
-      <ThemeTooltip title={themes[2].title}>
-        <div
-          className="rounded-full bg-[#558B68] h-6 w-6"
-          onClick={() => {
-            setTheme(themes[2].title);
-          }}
-        />
-      </ThemeTooltip>
-      <ThemeTooltip title={themes[3].title}>
-        <div
-          className="rounded-full bg-[#96705B] h-6 w-6"
-          onClick={() => {
-            setTheme(themes[3].title);
-          }}
-        />
-      </ThemeTooltip>
+    <div className="border-typography border-2 rounded-2xl p-2 text-typography inline-block">
+      Pick a theme!
+      <div className="space-x-4">
+        <ThemeTooltip title={themes[0].title}>
+          <div
+            className="rounded-full bg-[#EF6A80] h-6 w-6"
+            onClick={() => {
+              setTheme(themes[0].title);
+            }}
+          />
+        </ThemeTooltip>
+        <ThemeTooltip title={themes[1].title}>
+          <div
+            className="rounded-full bg-[#436FC7] h-6 w-6"
+            onClick={() => {
+              setTheme(themes[1].title);
+            }}
+          />
+        </ThemeTooltip>
+        <ThemeTooltip title={themes[2].title}>
+          <div
+            className="rounded-full bg-[#558B68] h-6 w-6"
+            onClick={() => {
+              setTheme(themes[2].title);
+            }}
+          />
+        </ThemeTooltip>
+        <ThemeTooltip title={themes[3].title}>
+          <div
+            className="rounded-full bg-[#96705B] h-6 w-6"
+            onClick={() => {
+              setTheme(themes[3].title);
+            }}
+          />
+        </ThemeTooltip>
+      </div>
+      <p className="text-sm opacity-90">
+        Note: I&apos;ll remember your chosen theme for the next time you return!
+      </p>
     </div>
   );
 }
